d5f5849977dfacb2cfb969f49dffce9447b42351
Lucas Guidi <lucasrguidi@gmail.com>
2026-01-27 15:42:51 -0300

    chore: reset default files

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -2,9 +2,8 @@ import { Geist, Geist_Mono } from 'next/font/google'
 
 import type { Metadata } from 'next'
 
-import '../index.css'
-import Header from '@/components/header'
 import Providers from '@/components/providers'
+import '../index.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -32,10 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>
-          <div className="grid h-svh grid-rows-[auto_1fr]">
-            <Header />
-            {children}
-          </div>
+          <div className="grid h-svh grid-rows-[auto_1fr]">{children}</div>
         </Providers>
       </body>
     </html>
